d3942b631ad27cf63b8e664295e6bb44074c7058
Ken <kenduen@gmail.com>
2024-10-04 22:44:01 +0900

    BuybackSearchToolを追加

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-// app/page.js
+import BuybackSearchTool from './BuybackSearchTool'; // BuybackSearchToolのファイルパスに応じて調整してください
+
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>こんにちは、Kaitori_2へようこそ！</h1>
-      <p>これはあなたのカスタムページです。</p>
-      {/* ここにコメントを追加してみます */}
+    <div>
+      <BuybackSearchTool />
     </div>
   );
 }
