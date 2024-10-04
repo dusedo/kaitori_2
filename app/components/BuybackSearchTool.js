114c21e771395f6bd0f8bbf981f68e72c221676f
Ken <kenduen@gmail.com>
2024-10-04 22:54:27 +0900

    Add BuybackSearchTool component

---
+++ app/components/BuybackSearchTool.js
@@ -0,0 +1,13 @@
+// components/BuybackSearchTool.js
+import React from 'react';
+
+const BuybackSearchTool = () => {
+  return (
+    <div>
+      <h2>Buyback Search Tool Component</h2>
+      {/* ここにコンポーネントの内容を追加します */}
+    </div>
+  );
+};
+
+export default BuybackSearchTool;
