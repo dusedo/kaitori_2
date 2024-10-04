b6536d2c4d893e1fc5601198d97f77877f784cac
kedu <77043279+dusedo@users.noreply.github.com>
2024-10-04 22:20:17 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>こんにちは、Kaitori_2へようこそ！</h1>
       <p>これはあなたのカスタムページです。</p>
+      {/* ここにコメントを追加してみます */}
     </div>
   );
 }
